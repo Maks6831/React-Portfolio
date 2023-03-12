c5d2c0031e80ff5e98a997b4899f11c3290fbbc6
Maks6831 <ma.khan6971@gmail.com>
2023-03-12 17:38:01 +0000

    Added fully functioning navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+
+
 
 const App = () => {
   return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/projects' element={<Projects/>} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </div>
 
+    </Router>
+  
+    
+    
 
   )
 }
