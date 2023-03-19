18d677ccfc8104b37f4627f67c9e211d9b507dd6
Maks6831 <ma.khan6971@gmail.com>
2023-03-19 16:24:23 +0000

    fixed Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar  = () => {
     return (
     <nav className="bg-gray-800">
   <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8"/>
-    <div className="relative flex h-16 items-center justify-between">
+    <div className="relative flex md:h-16 items-center justify-between">
       <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
       </div>
       <div className="flex flex-1 items-center flex-row-reverse justify-end sm:items-stretch sm:justify-start">
